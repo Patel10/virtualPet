dfbbe49e98998a53f679e28a39ff5f6459ac2b42
Patel10 <parth1029@yahoo.com>
2021-04-13 09:14:11 -0400

    update with tick method working

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,53 +1,73 @@
+package virtual_pet;
 public class VirtualPet {
-
+// tried my best to complete the coding assignment.
 
     private String name;
-    private int Hunger;
-    private int Sleepy;
-    private int Bored;
+    private int hunger;
+    private int sleepy;
+    private int bored;
 
-    public VirtualPet(String n ) {
-        name = n;
-        Sleepy = 1;
-        Hunger = 1;
-        Bored = 1;
+    public VirtualPet(String name, int hunger, int sleepy, int bored){
+
+
+        this.name = name;
+        this.sleepy = sleepy;
+        this.hunger = hunger;
+        this.bored = bored;
     }
 
-     public String getName(){
+    public String getName() {
         return name;
     }
-    public int getHunger(){
-        return Hunger;
-    }
-    public int getSleepy(){
-        return Sleepy;
-    }
-    public int getBored(){
-        return Bored;
-    }
-public void setHunger(int h) {
-    Hunger = h;
-}
-public void setSleepy(int s) {
-    Sleepy = s;
-}
-public void setBored(int b) {
-    Bored = b;
-}
-public void modifyHunger ( int h) {
-    Hunger += h;
-}
-public static void modifySleepy  ( int s) {
-    Sleepy += s;
-}
-public void modifyBored ( int b) {
-    Bored += b;
-}
 
-public void tick() {
-    Sleepy += 1;
-    Hunger += 1;
-    Bored += 1;
-}
+    public int getHunger() {
+        return hunger;
+    }
+
+    public int getSleepy() {
+        return sleepy;
+    }
+
+    public int getBored() {
+        return bored;
+    }
+
+
+
+    public void setHunger(int h) {
+        hunger = h;
+    }
+
+    public void setSleepy(int s) {
+        sleepy = s;
+    }
+
+    public void setBored(int b) {
+        bored = b;
+    }
+
+    public void modifyHunger(int h) {
+        hunger += h;
+    }
+
+    public void updateSleepy(int s) {
+        sleepy += s;
+    }
+
+    public void updateHunger(int h) {
+        bored += h;
+    }
+    public void updateBored(int b ) {
+
+    }
+
+    public void tick() {
+        updateSleepy(+3);
+        updateHunger(+3);
+        updateBored(+3);
+
+
+
+    }
 
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,5 +1,6 @@
 package virtual_pet;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class VirtualPetApplication {
@@ -7,14 +8,27 @@ public class VirtualPetApplication {
     public static void main(String[] args) {
         //Interact with a VirtualPet object in this method
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 0 - 3");
+        System.out.println("[0] Continue ");
+        System.out.println("[1] Feed Clelion");
+        System.out.println("[2] Play with");
+        System.out.println("[3] Take for a walk");
+        System.out.println("[4] Press 4 to check current status");
 
-        System.out.println("Clelion");
+        String select = input.nextLine();
+
+
+
+
+        System.out.println("Enter a number to begin interaction with Clelion ");
+
         String name = input.nextLine();
 
-        VirtualPet = newVirtualPet(Clelion);
-        System.out.println("Hi" + pet.getName() + "Go for it");
+        VirtualPet myVirtualPet = new VirtualPet ("Clelion", 0, 0, 0);
 
-        int select;
+        System.out.println("Hi, I'm " + myVirtualPet.getName() + " Go for it");
+
+
 
         do {
             System.out.println("Enter a number between 0 - 3");
@@ -22,54 +36,65 @@ public class VirtualPetApplication {
             System.out.println("[1] Feed Clelion");
             System.out.println("[2] Play with");
             System.out.println("[3] Take for a walk");
+            System.out.println("[4] Press 4 to check current status");
 
             select = input.nextLine();
 
-            if ( select == 0) {
+            if ( select.equals("0")) {
                 continue;
             }
                 // 1 is Feed
-        else if ( select == 1 ) {
+        else if ( select.equals("1") ) {
 
-                if (pet.getHunger() - 1 < 0) {
+                if (myVirtualPet.getHunger() - 1 < 0) {
                     System.out.println("It's reaching that point, give me a break");
+                    myVirtualPet.updateHunger(-2);
                 continue;
             }
-                if (pet.getSleepy() + 2 > 7) {
+                if (myVirtualPet.getSleepy() + 2 > 7) {
                     System.out.println("Getting bit overworked, need a break.");
+                    myVirtualPet.updateSleepy(-2);
                     continue;
                 }
                 System.out.println("Yum yummm, liking the menu today");
-            } // 2 is Play
-        else if ( select == 2) {
+            } // 2 is Play'
+        else if ( select.equals("2")) {
 
-                if (pet.getHunger() + 2 > 7) {
+                if (myVirtualPet.getHunger() + 2 > 7) {
                     System.out.println("Moving around, toss me a hotdog");
+                    myVirtualPet.updateHunger(-2);
                     continue;
                 }
-                if (pet.getSleepy() + 2 > 7) {
+                if (myVirtualPet.getSleepy() + 2 > 7) {
                     System.out.println(" I'm exhausted ");
                     continue;
                 }
                 System.out.println("Let's play fetch");
             }  // 3 is take for walk
-        else if ( select == 3) {
+        else if ( select.equals("3") ) {
 
-                if (pet.getBored() + 2 > 7) {
+                if (myVirtualPet.getBored() + 2 > 7) {
                     System.out.println("Bored, let's go to park");
                     continue;
                 }
                 System.out.println(" Let's go to park");
 
-                String[] food = new String[]{"Carrot.", "Apple.", "Chicken."};
-                System.out.println(food[rand.nextInt(food.length)]);
+
             }
+        else if ( select.equals("4") ) {
+
+                System.out.println("Hunger: " + myVirtualPet.getHunger());
+                System.out.println("Sleepy: " + myVirtualPet.getSleepy());
+                System.out.println("Bored: " + myVirtualPet.getBored());
+            }
+
+
             else {
                 System.out.println("WRONG NUMBER.Only select provided numbers.");
             }
-             pet.tick();
+             myVirtualPet.tick();
 
-        } while (select == 0 );
+        } while (!select.equals("0") );
         }
     }
 
